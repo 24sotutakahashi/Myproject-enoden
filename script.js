e1ebd6c76fe15f54cd8f1852098404d7efe93a4e
Takashin <takas4n@gmail.com>
2024-01-16 20:22:37 +0900

    要素の追加 配置の変更

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
 // ドキュメントが完全に読み込まれた後に実行されるイベントリスナー
 document.addEventListener('DOMContentLoaded', function () {
     // スタート用のボタン要素を取得
+    // スタート用のボタン要素を取得
     const startBuildingButtons = document.getElementById('startBuildingButtons');
+
     const startBuildingButton = document.getElementById('startBuildingButton');
     const startBuilding2Button = document.getElementById('startBuilding2Button');
     const startBuilding3Button = document.getElementById('startBuilding3Button');
     const startBuilding4Button = document.getElementById('startBuilding4Button');
     const startBuilding5Button = document.getElementById('startBuilding5Button');
+    const startBuilding6Button = document.getElementById('startBuilding6Button');
+    const startBuilding7Button = document.getElementById('startBuilding7Button');
+    const startBuilding8Button = document.getElementById('startBuilding8Button');
+    const startBuilding9Button = document.getElementById('startBuilding9Button');
+    const startBuilding10Button = document.getElementById('startBuilding10Button');
+    const startBuilding11Button = document.getElementById('startBuilding11Button');
+    const startBuilding12Button = document.getElementById('startBuilding12Button');
+    const startBuilding13Button = document.getElementById('startBuilding13Button');
+    const startBuilding14Button = document.getElementById('startBuilding14Button');
+    const startBuilding15Button = document.getElementById('startBuilding15Button');
+    const startBuilding16Button = document.getElementById('startBuilding16Button');
+    const startBuilding17Button = document.getElementById('startBuilding17Button');
+    const startBuilding18Button = document.getElementById('startBuilding18Button');
+    const startBuilding19Button = document.getElementById('startBuilding19Button');
+    const startBuilding20Button = document.getElementById('startBuilding20Button');
+
 
 
 
@@ -18,10 +36,28 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // ゴール用のボタン要素を取得
     const goalBuildingButtons = document.getElementById('goalBuildingButtons');
+
     const goalBuildingButton = document.getElementById('goalBuildingButton');
     const goalBuilding2Button = document.getElementById('goalBuilding2Button');
     const goalBuilding3Button = document.getElementById('goalBuilding3Button');
     const goalBuilding4Button = document.getElementById('goalBuilding4Button');
+    const goalBuilding5Button = document.getElementById('goalBuilding5Button');
+    const goalBuilding6Button = document.getElementById('goalBuilding6Button');
+    const goalBuilding7Button = document.getElementById('goalBuilding7Button');
+    const goalBuilding8Button = document.getElementById('goalBuilding8Button');
+    const goalBuilding9Button = document.getElementById('goalBuilding9Button');
+    const goalBuilding10Button = document.getElementById('goalBuilding10Button');
+    const goalBuilding11Button = document.getElementById('goalBuilding11Button');
+    const goalBuilding12Button = document.getElementById('goalBuilding12Button');
+    const goalBuilding13Button = document.getElementById('goalBuilding13Button');
+    const goalBuilding14Button = document.getElementById('goalBuilding14Button');
+    const goalBuilding15Button = document.getElementById('goalBuilding15Button');
+    const goalBuilding16Button = document.getElementById('goalBuilding16Button');
+    const goalBuilding17Button = document.getElementById('goalBuilding17Button');
+    const goalBuilding18Button = document.getElementById('goalBuilding18Button');
+    const goalBuilding19Button = document.getElementById('goalBuilding19Button');
+    const goalBuilding20Button = document.getElementById('goalBuilding20Button');
+
 
 
     const goalFloorButtons = document.getElementById('goalFloorButtons');
@@ -69,12 +105,29 @@ document.addEventListener('DOMContentLoaded', function () {
     // 各建物ごとに表示する階を定義するオブジェクト
     // 例: 1号館は1階から3階まであるので、'1': ['1', '2', '3'] と定義
     const buildingFloors = {
-        '1': ['1', '2', '3'],
-        '2': ['1', '2', '3', '4'],
-        '3': ['1', '2', '3'],
-        '4': ['1', '2', '3', '4'],
-        '5': ['1', '2', '3', '4', '5']
+        "1": ["1", "2", "3"],
+        "2": ["1", "2", "3", "4"],
+        "3": ["1", "2", "3"],
+        "4": ["1", "2", "3", "4"],
+        "5": ["1", "2", "3", "4"],
+        "6": ["1", "2", "3", "4", "5", "6"],
+        "7": ["1", "2"],
+        "8": ["1", "2", "3", "4", "5"],
+        "9": ["1", "2", "3"],
+        "10": ["1", "2", "3", "4", "5"],
+        "11": ["1"], // 11号館は1階のみ
+        "12": ["1"], // 12号館は1階のみ
+        "13": ["1"], // 13号館は1階のみ
+        "14": ["1"], // 14号館は1階のみ
+        "15": ["1"], // 15号館は1階のみ
+        "16": ["1"], // 16号館は1階のみ
+        "17": ["1"], // 17号館は1階のみ
+        "18": ["1", "2", "3"],
+        "19": ["1"], // 19号館は1階のみ
+        "20": ["1"],
     };
+
+
 
     function updateFloorButtons(building) {
         // 選択された建物の階を取得
@@ -94,55 +147,38 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     // イベントリスナーを建物のボタンに追加
-    startBuildingButton.addEventListener('click', () => {
-        selectedBuilding = '1';
-        hideButtonContainer(startBuildingButtons);
-        setTimeout(() => {
-            updateFloorButtons(selectedBuilding);
-            showButtonContainer(startFloorButtons);
-        }, 500);
-    });
+    function addBuildingButtonListener(button, buildingNumber) {
+        button.addEventListener('click', () => {
+            selectedBuilding = buildingNumber;
+            hideButtonContainer(startBuildingButtons);
+            setTimeout(() => {
+                updateFloorButtons(selectedBuilding);
+                showButtonContainer(startFloorButtons);
+            }, 500);
+        });
+    }
+    // 各建物ボタンにリスナーを追加
+    addBuildingButtonListener(startBuildingButton, '1');
+    addBuildingButtonListener(startBuilding2Button, '2');
+    addBuildingButtonListener(startBuilding3Button, '3');
+    addBuildingButtonListener(startBuilding4Button, '4');
+    addBuildingButtonListener(startBuilding5Button, '5');
+    addBuildingButtonListener(startBuilding6Button, '6');
+    addBuildingButtonListener(startBuilding7Button, '7');
+    addBuildingButtonListener(startBuilding8Button, '8');
+    addBuildingButtonListener(startBuilding9Button, '9');
+    addBuildingButtonListener(startBuilding10Button, '10');
+    addBuildingButtonListener(startBuilding11Button, '11');
+    addBuildingButtonListener(startBuilding12Button, '12');
+    addBuildingButtonListener(startBuilding13Button, '13');
+    addBuildingButtonListener(startBuilding14Button, '14');
+    addBuildingButtonListener(startBuilding15Button, '15');
+    addBuildingButtonListener(startBuilding16Button, '16');
+    addBuildingButtonListener(startBuilding17Button, '17');
+    addBuildingButtonListener(startBuilding18Button, '18');
+    addBuildingButtonListener(startBuilding19Button, '19');
+    addBuildingButtonListener(startBuilding20Button, '20');
 
-    startBuilding2Button.addEventListener('click', () => {
-        selectedBuilding = '2';
-        hideButtonContainer(startBuildingButtons);
-        setTimeout(() => {
-            updateFloorButtons(selectedBuilding);
-            showButtonContainer(startFloorButtons);
-        }, 500);
-    });
-
-    startBuilding3Button.addEventListener('click', () => {
-        selectedBuilding = '3';
-        hideButtonContainer(startBuildingButtons);
-        setTimeout(() => {
-            updateFloorButtons(selectedBuilding);
-            showButtonContainer(startFloorButtons);
-        }, 500);
-    });
-
-
-
-
-    startBuilding4Button.addEventListener('click', () => {
-        selectedBuilding = '4';
-        hideButtonContainer(startBuildingButtons);
-        setTimeout(() => {
-            updateFloorButtons(selectedBuilding);
-            showButtonContainer(startFloorButtons);
-        }, 500);
-    });
-
-
-    // イベントリスナーを建物のボタンに追加
-    startBuilding5Button.addEventListener('click', () => {
-        selectedBuilding = '5';
-        hideButtonContainer(startBuildingButtons);
-        setTimeout(() => {
-            updateFloorButtons(selectedBuilding);
-            showButtonContainer(startFloorButtons);
-        }, 500);
-    });
 
 
 
@@ -208,41 +244,41 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }
 
-    goalBuildingButton.addEventListener('click', () => {
-        selectedBuildingForGoal = '1';
-        hideButtonContainer(goalBuildingButtons);
-        setTimeout(() => {
-            updateFloorButtonsForgoal(selectedBuildingForGoal);
-            showButtonContainer(goalFloorButtons);
-        }, 500);
-    });
 
-    goalBuilding2Button.addEventListener('click', () => {
-        selectedBuildingForGoal = '2';
-        hideButtonContainer(goalBuildingButtons);
-        setTimeout(() => {
-            updateFloorButtonsForgoal(selectedBuildingForGoal);
-            showButtonContainer(goalFloorButtons);
-        }, 500);
-    });
 
-    goalBuilding3Button.addEventListener('click', () => {
-        selectedBuildingForGoal = '3';
-        hideButtonContainer(goalBuildingButtons);
-        setTimeout(() => {
-            updateFloorButtonsForgoal(selectedBuildingForGoal);
-            showButtonContainer(goalFloorButtons);
-        }, 500);
-    });
 
-    goalBuilding4Button.addEventListener('click', () => {
-        selectedBuildingForGoal = '4';
-        hideButtonContainer(goalBuildingButtons);
-        setTimeout(() => {
-            updateFloorButtonsForgoal(selectedBuildingForGoal);
-            showButtonContainer(goalFloorButtons);
-        }, 500);
-    });
+    function addGoalBuildingButtonListener(button, buildingNumber) {
+        button.addEventListener('click', () => {
+            selectedBuildingForGoal = buildingNumber;
+            hideButtonContainer(goalBuildingButtons);
+            setTimeout(() => {
+                updateFloorButtonsForgoal(selectedBuildingForGoal);
+                showButtonContainer(goalFloorButtons);
+            }, 500);
+        });
+    }
+
+    addGoalBuildingButtonListener(goalBuildingButtons, '1');
+    addGoalBuildingButtonListener(goalBuilding2Button, '2');
+    addGoalBuildingButtonListener(goalBuilding3Button, '3');
+    addGoalBuildingButtonListener(goalBuilding4Button, '4');
+    addGoalBuildingButtonListener(goalBuilding5Button, '5');
+    addGoalBuildingButtonListener(goalBuilding6Button, '6');
+    addGoalBuildingButtonListener(goalBuilding7Button, '7');
+    addGoalBuildingButtonListener(goalBuilding8Button, '8');
+    addGoalBuildingButtonListener(goalBuilding9Button, '9');
+    addGoalBuildingButtonListener(goalBuilding10Button, '10');
+    addGoalBuildingButtonListener(goalBuilding11Button, '11');
+    addGoalBuildingButtonListener(goalBuilding12Button, '12');
+    addGoalBuildingButtonListener(goalBuilding13Button, '13');
+    addGoalBuildingButtonListener(goalBuilding14Button, '14');
+    addGoalBuildingButtonListener(goalBuilding15Button, '15');
+    addGoalBuildingButtonListener(goalBuilding16Button, '16');
+    addGoalBuildingButtonListener(goalBuilding17Button, '17');
+    addGoalBuildingButtonListener(goalBuilding18Button, '18');
+    addGoalBuildingButtonListener(goalBuilding19Button, '19');
+    addGoalBuildingButtonListener(goalBuilding20Button, '20');
+
 
 
 
